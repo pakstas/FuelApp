3296efa6aedef8c42a9a71b56a780c8c8dcfc36e
pakstas <pakstas@github.com>
2020-09-28 14:53:54 +0300

    added router rules

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import ViewCars from "../views/ViewCars.vue";
+import firebase from "firebase/app";
+import "firebase/auth";
 
 Vue.use(VueRouter);
 
@@ -9,34 +11,52 @@ const routes = [
     path: "/",
     name: "View Cars",
     component: ViewCars,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/login",
     name: "Login",
+    meta: {
+      requiresAnon: true,
+    },
     component: () =>
       import(/* webpackChunkName: "login" */ "../views/Login.vue"),
   },
   {
     path: "/register",
     name: "Register",
+    meta: {
+      requiresAnon: true,
+    },
     component: () =>
       import(/* webpackChunkName: "register" */ "../views/Register.vue"),
   },
   {
     path: "/addcar",
     name: "Add Car",
+    meta: {
+      requiresAuth: true,
+    },
     component: () =>
       import(/* webpackChunkName: "addcar" */ "../views/AddCar.vue"),
   },
   {
     path: "/viewfuel/:id",
     name: "View Fuel",
+    meta: {
+      requiresAuth: true,
+    },
     component: () =>
       import(/* webpackChunkName: "viewfuel" */ "../views/ViewFuel.vue"),
   },
   {
     path: "/addfuel/:id",
     name: "Add Fuel",
+    meta: {
+      requiresAuth: true,
+    },
     component: () =>
       import(/* webpackChunkName: "addfuel" */ "../views/AddFuel.vue"),
   },
@@ -46,6 +66,18 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (!user && to.matched.some((route) => route.meta.requiresAuth)) {
+      next({ path: "/login" });
+    } else if (user && to.matched.some((route) => route.meta.requiresAnon)) {
+      next({ path: "/" });
+    } else {
+      next();
+    }
+  });
 });
 
 export default router;
